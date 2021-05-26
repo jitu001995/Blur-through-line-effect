f88645cbdf625b88869ba89783e9627ce33aba60
jitu <k1995jitu@gmail.com>
2021-05-26 23:53:19 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const text=document.getElementById('text');
+const textArr=text.innerText.split('');
+
+console.log(textArr);
+
+const newEl=document.createElement('h1');
+
+newEl.innerHTML=`
+ ${textArr.map(letter =>
+    `<span style="${randomVisibility()}">${letter}</span>`
+).join('')}`;
+
+console.log(newEl);
+
+newEl.classList.add('overlay');
+
+document.body.appendChild(newEl);
+
+function randomVisibility(){
+    return Math.random() < 0.5 ? 'visiblity: hidden' : 'visibility:visible'
+}
